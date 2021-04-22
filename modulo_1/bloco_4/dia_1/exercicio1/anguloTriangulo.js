2dd66982babaee8fc97677e16c5a1318161a3e7c
José Carlos <kalarruda@hotmail.com>
2021-04-22 15:20:52 -0300

    Adiciona arquivo anguloTriangulo.js

---
+++ modulo_1/bloco_4/dia_1/exercicio1/anguloTriangulo.js
@@ -0,0 +1,12 @@
+let a = 30;
+let b = 60;
+let c = 30;
+
+let somaAngulos = a + b +c;
+
+if (somaAngulos==180){
+    console.log("O triângulo é válido. Valor total dos ângulos: "+somaAngulos);
+}
+else{
+    console.log("O triângulo NÃO é válido. Valor total dos ângulos: "+somaAngulos);
+}
