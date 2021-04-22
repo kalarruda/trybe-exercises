33d7a12bec0ebec8353e2a5f750361c62f01e839
José Carlos <kalarruda@hotmail.com>
2021-04-22 15:27:19 -0300

    Melhora o  arquivo anguloTriangulo.js

--- modulo_1/bloco_4/dia_1/exercicio1/anguloTriangulo.js
+++ modulo_1/bloco_4/dia_1/exercicio1/anguloTriangulo.js
@@ -1,12 +1,17 @@
 let a = 30;
 let b = 60;
-let c = 30;
+let c = -90;
 
 let somaAngulos = a + b +c;
+let valido = true;
+let invalido = false;
 
 if (somaAngulos==180){
-    console.log("O triângulo é válido. Valor total dos ângulos: "+somaAngulos);
+    console.log(valido);
+}
+else if(a<0 || b<0 || c<0 ){
+    console.log("ERRO. Um dos ângulos é negativo.");
 }
 else{
-    console.log("O triângulo NÃO é válido. Valor total dos ângulos: "+somaAngulos);
+    console.log(invalido);
 }
