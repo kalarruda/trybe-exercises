4301f91be0adfc00801d5ad6bda96ab395a2f38e
José Carlos <kalarruda@hotmail.com>
2021-04-22 12:29:37 -0300

    Adiciona programa de adiçao e subtraçao

---
+++ modulo_1/bloco_4/dia_1/exercicio1/adicao.js
@@ -0,0 +1,6 @@
+let a = 2;
+let b = 5;
+
+let adicao = a+b;
+
+console.log ("O valor da soma é: "+adicao);
---
+++ modulo_1/bloco_4/dia_1/subtracao.js
@@ -0,0 +1,6 @@
+let a = 4;
+let b = 6;
+
+let subtracao = a-b;
+
+console.log("O valor da subtração é: "+subtracao);
