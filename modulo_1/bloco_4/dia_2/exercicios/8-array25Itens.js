facc7dde3d2c5894013d5050f11080c01e82f6f6
José Carlos <kalarruda@hotmail.com>
2021-04-23 15:08:36 -0300

    Adiciona exercicio 8-array25Itens.js

--- modulo_1/bloco_4/dia_2/exercicios/8-array25Itens.js
+++ modulo_1/bloco_4/dia_2/exercicios/8-array25Itens.js
@@ -1,11 +1,17 @@
 //Utilizando for , crie uma array que vá de 1 até 25 e imprima o resultado;
-let valor =[];
+
+// let valor =[];
+// let lista =[];
+
+// for (let n=1 ; n<=25 ; n +=1){   
+//     lista = valor.push(n);
+//     console.log(lista);
+// }
+
+let valor=[];
 
 for (let n=1 ; n<=25 ; n +=1){   
-    valor=[n];
-    valor.push(valor[n]);
-    console.log(valor);
+    valor.push(n);
 }
 
-let lista = valor;
-    console.log(valor);
+console.log(valor);
