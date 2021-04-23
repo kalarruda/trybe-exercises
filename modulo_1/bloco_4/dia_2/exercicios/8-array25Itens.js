7257d4329f1a5928aaa9715ac84e9022f940df4d
José Carlos <kalarruda@hotmail.com>
2021-04-23 14:48:25 -0300

    Teste Debug que deu errado

---
+++ modulo_1/bloco_4/dia_2/exercicios/8-array25Itens.js
@@ -0,0 +1,11 @@
+//Utilizando for , crie uma array que vá de 1 até 25 e imprima o resultado;
+let valor =[];
+
+for (let n=1 ; n<=25 ; n +=1){   
+    valor=[n];
+    valor.push(valor[n]);
+    console.log(valor);
+}
+
+let lista = valor;
+    console.log(valor);
