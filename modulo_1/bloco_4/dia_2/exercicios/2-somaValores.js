f660008f09fc064d82e3664c37d7e31d38ac6b14
José Carlos <kalarruda@hotmail.com>
2021-04-23 12:12:51 -0300

    Adiciona exercicio 3-mediaAritmetica.js

--- modulo_1/bloco_4/dia_2/exercicios/2-somaValores.js
+++ modulo_1/bloco_4/dia_2/exercicios/2-somaValores.js
@@ -4,7 +4,7 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let calculo=0;
 
 for (soma=0 ; soma<numbers.length ; soma +=1){
-    calculo = calculo + numbers[soma];
-    
+    calculo = calculo + numbers[soma];    
 }
+
 console.log(calculo);
