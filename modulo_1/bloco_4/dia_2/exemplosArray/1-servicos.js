5fea00ba492f5bb3ae0d8102b3afd1d826d510ac
José Carlos <kalarruda@hotmail.com>
2021-04-23 11:31:31 -0300

    Muda nome da pasta exemplos para exemplosArray

---
+++ modulo_1/bloco_4/dia_2/exemplosArray/1-servicos.js
@@ -0,0 +1,21 @@
+//Obtenha o valor "Serviços" do array menu :
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+
+console.log(menuServices);
+
+
+
+
+
+// let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+// tasksList.unshift("primeiro");
+// tasksList.push('Comida');
+// tasksList.pop();
+// tasksList.shift();
+// let lista = tasksList[tasksList.length -1];
+// let posicao = tasksList.indexOf("reunião");
+// console.log(posicao);
+
+
