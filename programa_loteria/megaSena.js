8cc4ef0e9eb9c98d17391baf739b0ff12f6b6aa5
José Carlos <kalarruda@hotmail.com>
2021-04-24 17:32:54 -0300

    Loteria finalizado

--- programa_loteria/megaSena.js
+++ programa_loteria/megaSena.js
@@ -9,15 +9,16 @@ randomNumber6 = Math.ceil(Math.random()*60);
 let megaSena = [randomNumber1, randomNumber2, randomNumber3, randomNumber4, randomNumber5,randomNumber6];
 
 let aposta1 = [1 , 2, 3, 4 ,5 ,6];
-let aposta2 = [22 ,6, 11, 8 ,5 ,1];
+let aposta2 = [22 ,60, 11, 8 ,15 ,10];
 
 let totalApostas = [aposta1, aposta2];
 
-for (totaisJogos = 0 ; totaisJogos < totalApostas.length ; totaisJogos +=1){//lista de jogos
+
+for (let totaisJogos = 0 ; totaisJogos < totalApostas.length ; totaisJogos +=1){//lista de jogos
     let acertos = 0;
-    for (jogoMega = 0 ; jogoMega < megaSena.length ; jogoMega += 1){//números mega sena
-        for(jogoAposta = 0 ; jogoAposta < aposta1.length ; jogoAposta +=1){//números de todos os jogos
-            if (aposta2[jogoAposta]===megaSena[jogoMega]){
+    for (let jogoMega = 0 ; jogoMega < megaSena.length ; jogoMega += 1){//números mega sena
+        for(let jogoAposta = 0 ; jogoAposta < totalApostas[totaisJogos].length ; jogoAposta +=1){//números de todos os jogos
+            if (totalApostas[totaisJogos][jogoAposta] === megaSena[jogoMega]){
                 acertos +=1;
             };
         };
@@ -27,6 +28,22 @@ console.log (`Valores apostados: ${totalApostas[totaisJogos]}`);
 console.log(`jogo da mega sena: ${megaSena}`);
 console.log (`Número acertos: ${acertos}`);
 console.log(`------------------------------`);
-
 };
 
+// PRIMEIRA TENTATIVA ERRADA ------------------------------------------------------------
+
+// for (let totaisJogos = 0 ; totaisJogos < totalApostas.length ; totaisJogos +=1){//lista de jogos
+//     let acertos = 0;
+//     for (let jogoMega = 0 ; jogoMega < megaSena.length ; jogoMega += 1){//números mega sena
+//         for(let jogoAposta = 0 ; jogoAposta < aposta1.length ; jogoAposta +=1){//números de todos os jogos
+//             if (totalApostas[jogoAposta]===megaSena[jogoMega]){
+//                 acertos +=1;
+//             };
+//         };
+
+//     };
+// console.log (`Valores apostados: ${totalApostas[totaisJogos]}`);
+// console.log(`jogo da mega sena: ${megaSena}`);
+// console.log (`Número acertos: ${acertos}`);
+// console.log(`------------------------------`);
+// };
