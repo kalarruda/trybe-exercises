ae10d8e683764cbd309e737f8bf0cd790b8e2664
José Carlos <kalarruda@hotmail.com>
2021-04-25 19:04:39 -0300

    1-exercicioAsteristico.js

---
+++ modulo_1/bloco_4/dia_3/1-exercicioAsteristicos.js
@@ -0,0 +1,20 @@
+let number = 5;
+
+
+for (let line=0 ; line<number ; line +=1){//2
+  let asteristico =``;
+  for (col=0 ; col < number ; col +=1){
+    asteristico += `*`;    
+  };
+  console.log(asteristico);
+};
+
+
+// let colunas = 7;
+
+// for(let l = 0; l < colunas; l++){
+//     for(let c = 0; c < colunas; c++){
+//         console.log("*"); // colunas
+//     }
+//     console.log(); // linhas
+// }
