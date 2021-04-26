f176dc538b0363b8868afe57c9bc0f08d02b4903
José Carlos <kalarruda@hotmail.com>
2021-04-25 21:38:07 -0300

    3-exercicioTrianguloInvertido.js e muda adiciona informacao nos exercicios 1 e 2

--- modulo_1/bloco_4/dia_3/1-exercicioAsteristicos.js
+++ modulo_1/bloco_4/dia_3/1-exercicioAsteristicos.js
@@ -1,9 +1,15 @@
-let number = 5;
+//Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n . Por exemplo:
+// *****
+// *****
+// *****
+// *****
+// *****
 
+let number = 5;
 
 for (let line=0 ; line<number ; line +=1){//2
   let asteristico =``;
-  for (col=0 ; col < number ; col +=1){
+  for (let col=0 ; col < number ; col +=1){
     asteristico += `*`;    
   };
   console.log(asteristico);
