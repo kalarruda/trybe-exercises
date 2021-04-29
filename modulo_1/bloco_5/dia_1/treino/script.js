02486b2c0893ad846367db72ad398542bda7469e
José Carlos <kalarruda@hotmail.com>
2021-04-29 17:48:19 -0300

    2-treino adiciona estilo na pagina via DOM

---
+++ modulo_1/bloco_5/dia_1/treino/script.js
@@ -0,0 +1,18 @@
+
+document.getElementsByTagName("h1")[0].style.backgroundColor = "green";
+
+document.querySelector("#footer-container").style.backgroundColor = "green";
+
+document.querySelector(".emergency-tasks").style.backgroundColor = "pink";
+
+document.querySelector(".no-emergency-tasks").style.backgroundColor = "yellow";
+
+document.getElementsByTagName("h3")[0].style.backgroundColor = "blue";
+
+document.getElementsByTagName("h3")[1].style.backgroundColor = "blue";
+
+document.getElementsByTagName("h3")[2].style.backgroundColor = "black";
+
+document.getElementsByTagName("h3")[3].style.backgroundColor = "black";
+
+document.getElementsByTagName("body")[1].style.backgroundColor = "blue";
