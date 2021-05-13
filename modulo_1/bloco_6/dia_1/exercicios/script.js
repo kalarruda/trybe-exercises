a031aa01cf99399295d845e3f25287aea592e370
José Carlos <kalarruda@hotmail.com>
2021-05-13 10:07:58 -0300

    tentando novamente fazer parte 3 do 6.1

--- modulo_1/bloco_6/dia_1/exercicios/script.js
+++ modulo_1/bloco_6/dia_1/exercicios/script.js
@@ -1,3 +1,7 @@
+function handleSubmit(event) {
+  event.preventDefault();
+}
+
 const states = document.getElementById('state');
 
 const statesList = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
@@ -8,3 +12,66 @@ for (let index = 0; index < statesList.length; index += 1) {
   states.appendChild(options);
 }
 
+
+// let valor = '10/02/21';
+// let valorArray = valor.split('/');
+// console.log(valorArray);
+
+let valorData = document.getElementById('date').value;
+let valorArray2 = valorData.split('/');
+let botao = document.getElementById('btn');
+let arrayData = [];
+
+botao.addEventListener('click', function(event) {
+  arrayData = valorArray2;
+  return `${arrayData[2]}`;
+});
+
+
+// function validaDate(valor) {
+//   nova = valor.split('/');
+//   let dia = nova[0];
+//   let mes = nova[1];
+//   let ano = nova[2];
+
+//   return `${dia} ${mes} ${ano} `;
+
+//   // let data_array = valor.split('/');
+//   // let dia = data_array[0];
+//   // let mes = data_array[1];
+//   // let ano = data_array[2];
+
+//   // return console.log(nova);
+//   return ` ${dia}`;
+//   // console.log(mes);
+//   // console.log(ano);
+// }
+
+
+
+botao.addEventListener('click', function (e) {
+  e.preventDefault();
+});
+
+// let data = '10/02/2021';
+// let barra = '/';
+
+// // console.log(nova);
+// validaDate(nova, barra);
+
+
+
+
+
+//+++++++++++++++++++++++
+
+// const separador = input.value.split('/');
+// const dia = separador[0];
+// const mes = separador[1];
+// const ano = separador[2];
+// console.log(separador[0]);
+
+// function verificaData() {
+//   let diaInicial = document.getElementById('date');
+
+// }
