893f1f3c602f8f980255234577bc4169a04a5dcf
José Carlos <kalarruda@hotmail.com>
2021-05-26 18:20:50 -0300

    treino 4 e 5

--- modulo_1/bloco_8/dia_2/4-treino.js
+++ modulo_1/bloco_8/dia_2/4-treino.js
@@ -2,6 +2,6 @@
 
 const numbers = [19, 21, 30, 3, 45, 22, 15];
 
-const findDivisibleBy3And5 = numbers.find((result) => result % 2 === 0 && result % 5 === 0);
+const findDivisibleBy3And5 = numbers.find((result) => result % 3 === 0 && result % 5 === 0);
 
 console.log(findDivisibleBy3And5);
