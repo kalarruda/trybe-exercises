04ab180833a39526fc1770f76a95a7afcfdcb407
José Carlos <kalarruda@hotmail.com>
2021-05-27 16:03:04 -0300

    2-exercicio dificil de entender

--- modulo_1/bloco_8/dia_3/exercicios/2-exercicio.js
+++ modulo_1/bloco_8/dia_3/exercicios/2-exercicio.js
@@ -63,4 +63,40 @@ const books = [
   },
 ];
 
-// Adicione o código do exercício aqui:
+// 2 - Construa um array de objetos a partir do array de livros. Cada objeto deve conter uma propriedade author , com o nome da pessoa autora do livro, e uma propriedade age com a idade dessa pessoa quando o livro foi lançado. O array deve ser ordenado por idade, ou seja, da pessoa mais jovem para a mais velha considerando suas idades quando o livro foi lançado.
+// Dica: use as funções map , sort
+
+function nameAndAge() {
+  return books.map(info => ({ 
+    age: info.releaseYear - info.author.birthYear, author: info.author.name, })).sort((year1, year2) => year1.age - year2.age);
+}
+console.log(nameAndAge());
+
+const expectedResult = [
+  {
+    age: 31,
+    author: 'Isaac Asimov',
+  },
+  {
+    age: 38,
+    author: 'H. P. Lovecraft',
+  },
+  {
+    age: 39,
+    author: 'Stephen King',
+  },
+  {
+    age: 43,
+    author: 'George R. R. Martin',
+  },
+  {
+    age: 45,
+    author: 'Frank Herbert',
+  },
+  {
+    age: 62,
+    author: 'J. R. R. Tolkien',
+  },
+];
+
+assert.deepStrictEqual(nameAndAge(), expectedResult);
