84277ba19ef746d52b53e17c8183c3daa8d94f37
José Carlos <kalarruda@hotmail.com>
2021-06-26 10:16:21 -0300

    adiciona arquivo que tinha perdido dos fancy buttons

--- modulo_2/bloco_12/dia_1/fancy-buttons/src/App.js
+++ modulo_2/bloco_12/dia_1/fancy-buttons/src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.handleButtonOne = this.handleButtonOne.bind(this);
+    this.handleButtonTwo = this.handleButtonTwo.bind(this);
+    this.handleButtonThree = this.handleButtonThree.bind(this);
+
+    this.state = {
+      clicksBtnOne: 0,
+      clicksBtnTwo: 0,
+      clicksBtnThree: 0,
+    };
+  }
+
+  handleButtonOne() {
+    this.setState((prevState) => ({
+      clicksBtnOne: prevState.clicksBtnOne + 1,
+    }));
+  }
+  
+  handleButtonTwo() {
+    this.setState((prevState) => ({
+      clicksBtnTwo: prevState.clicksBtnTwo + 1,
+    }));
+  }
+  
+  handleButtonThree() {
+    // A única coisa diferente é a desconstrução.
+    // Apenas um método diferente de fazer a mesma coisa
+    // do que foi feito nas funções anteriores.
+    this.setState(({ clicksBtnThree }) => ({
+      clicksBtnThree: clicksBtnThree + 1,
+    }));
+  }
+  
+  render() {
+    const { clicksBtnOne, clicksBtnTwo, clicksBtnThree } = this.state;
+    console.log(this.state.clicksBtnOne);
+    return (
+      <div>
+        <button onClick={ this.handleButtonOne }>Botão 1 | Count = {clicksBtnOne}</button>
+        <button onClick={ this.handleButtonTwo }>Botão 2 | Count = {clicksBtnTwo}</button>
+        <button onClick={ this.handleButtonThree }>Botão 3 | Count = {clicksBtnThree}</button>
+      </div>
+    );
+  }
 }
 
 export default App;
