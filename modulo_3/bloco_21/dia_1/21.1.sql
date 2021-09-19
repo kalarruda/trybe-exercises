6ad085b08e893173cbd6bd19dbcfafff718bf038
José Carlos <kalarruda@hotmail.com>
2021-09-19 12:50:50 -0300

    ex 21.1

--- modulo_3/bloco_21/dia_1/21.1.sql
+++ modulo_3/bloco_21/dia_1/21.1.sql
@@ -55,5 +55,3 @@ FROM sakila.film AS t1, sakila.film AS t2
 WHERE t1.rental_duration = t2.rental_duration
 HAVING t1.rental_duration BETWEEN 2 AND 4;
 -- Exiba o título e a duração de empréstimo dos filmes que possuem a mesma duração. Exiba apenas os filmes com a duração de empréstimo entre 2 e 4 dias.
-
-
