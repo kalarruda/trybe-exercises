a058e4ae7f39936338d1bb2a4cc47639bf571d94
José Carlos <kalarruda@hotmail.com>
2021-10-22 18:32:51 -0300

    bl 25 dia 3

---
+++ modulo_3/bloco_25/dia_3/fixacao25.1/tests/calculaSituacao.js
@@ -0,0 +1,27 @@
+const { expect } = require('chai');
+
+const calculaSituacao = require('../examples/calculaSituacao');
+
+describe('Quando a média for menor que 7', () => {
+  it('retorna "reprovado"', () => {
+    const resposta = calculaSituacao(4);
+
+    expect(resposta).equals('reprovado');
+  });
+});
+
+describe('Quando a média for maior do que 7', () => {
+  it('retorna "aprovado"', () => {
+    const resposta = calculaSituacao(9);
+
+    expect(resposta).equals('aprovado')
+  });
+});
+
+describe('Quando a média for igual a 7', () => {
+  it('retorna "aprovado"', () => {
+    const resposta = calculaSituacao(7);
+
+    expect(resposta).equals('aprovado');
+  })
+})
