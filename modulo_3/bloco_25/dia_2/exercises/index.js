a1391ff99494a3594c4abd76867aa22e8dc06025
José Carlos <kalarruda@hotmail.com>
2021-10-24 16:16:06 -0300

    simpsons

--- modulo_3/bloco_25/dia_2/exercises/index.js
+++ modulo_3/bloco_25/dia_2/exercises/index.js
@@ -11,3 +11,45 @@ function calculo(a, b, c) {
     resolve(resultado)
   })
 }
+
+// TESTES PARA VER O RESULTADO
+calculo(10, 10, 10)
+  .then((resolve) => console.log(resolve))
+  .catch((erro) => console.log(erro));
+
+calculo(10, 'a', 5)
+  .then((resolve) => console.log(resolve))
+  .catch((error) => console.log(error));
+
+calculo(1, 1, 1)
+  .then((result) => console.log(result))
+  .catch((error) => console.log(error));
+
+function randomNumber() {
+  return Math.floor(Math.random() * 100 + 1);
+}
+// USANDO O THEN =======================
+
+// function testArrayNumbers() {
+//   const numbers = Array.from({ length: 3 }).map(randomNumber);
+//   console.log(numbers);
+//   calculo(...numbers)
+//   .then((resolve) => console.log(resolve))
+//   .catch((error) => console.log(error));
+// }
+
+// USANDO O ASYNC AWAIT===================
+
+async function testArrayNumbers() {
+  const numbers = Array.from({ length: 3 }).map(randomNumber);
+  console.log(numbers);
+  // const calc = await calculo(...numbers);
+  try {
+    const resolve = await calculo(...numbers);
+    console.log(resolve);
+  }
+  catch (error) {
+    console.error(error);
+  }
+}
+testArrayNumbers()
