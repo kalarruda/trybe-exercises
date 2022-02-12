602e0d2eda925cda125304b2ed8f725dce12dd1f
José Carlos <kalarruda@hotmail.com>
2022-02-12 11:27:24 -0300

    exercicio 1

--- modulo_4/bloco_32/dia_2/fixacao/tempCodeRunnerFile.py
+++ modulo_4/bloco_32/dia_2/fixacao/tempCodeRunnerFile.py
@@ -1,16 +1,1 @@
-import json
-
-with open("pokemons.json") as pokemons_file:
-    # conteudo = pokemons_file.read()
-    pokemons = json.load(pokemons_file)["results"]
-
-pokemons_type = [
-    pokemon
-    for pokemon in pokemons if "Grass" in pokemon["type"]
-]
-
-with open("grass_pokemons.json", "w") as arquivo_grama:
-    escrever_em_json = json.dumps(
-        pokemons_type
-    )
-arquivo_grama.write(escrever_em_json)
+arquivo
