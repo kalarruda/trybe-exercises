049b6efb44c2eca7c9e97283ed2a5e21c5e0fe82
José Carlos <kalarruda@hotmail.com>
2022-03-10 20:23:51 -0300

    treino1

---
+++ modulo_4/bloco_34/dia_3/fixacao/fixacao3.py
@@ -0,0 +1,14 @@
+import requests
+
+# com delay/10 - Por 10 segundos não temos certeza se a requisição irá retornar
+# response = requests.get("https://httpbin.org/delay/10")
+# print(response)
+
+try:
+    # recurso demora muito a responder
+    response = requests.get("http://httpbin.org/delay/10", timeout=2)
+except requests.ReadTimeout:
+    # vamos fazer uma nova requisição
+    response = requests.get("http://httpbin.org/delay/1", timeout=2)
+finally:
+    print(response.status_code)
