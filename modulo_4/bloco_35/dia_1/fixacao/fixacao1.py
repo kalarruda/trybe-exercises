7ded36007ced5853bafc96c494788630c23ef79c
José Carlos <kalarruda@hotmail.com>
2022-03-15 19:54:06 -0300

    fixacao 1

---
+++ modulo_4/bloco_35/dia_1/fixacao/fixacao1.py
@@ -0,0 +1,28 @@
+# def square(numbers):
+#     # tem que ser array
+#     array = []
+#     for number in numbers:
+#         array.append(number * number)
+#     return array
+
+#     square([3])
+
+
+def multiply_arrays(array1, array2):
+    result = []
+    number_of_iterations = 0
+
+    for number1 in array1:
+        print(f'Array 1: {number1}')
+        for number2 in array2:
+            print(f'Array 2: {number2}')
+            result.append(number1 * number2)
+            number_of_iterations += 1
+
+    print(f'{number_of_iterations} iterações!')
+    return result
+
+
+# meu_array = [1,2,3,4,5]
+
+# multiply_arrays(meu_array, meu_array)
