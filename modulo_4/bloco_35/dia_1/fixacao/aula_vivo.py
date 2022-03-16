dddc8cb0cb06b0c1718578134dd10f7081e1f16c
José Carlos <kalarruda@hotmail.com>
2022-03-16 19:10:42 -0300

    nao consegui fazer funcionar

---
+++ modulo_4/bloco_35/dia_1/fixacao/aula_vivo.py
@@ -0,0 +1,13 @@
+def mais_que_vinte_cinco_porcento(array):
+    vinte_cinco_porcento = len(array)
+    setenta_cinco_porcento = len(array) - vinte_cinco_porcento
+    for index, elemento in enumerate(array[:setenta_cinco_porcento]):
+        if elemento == array[index + vinte_cinco_porcento]:
+            return elemento
+    return -1
+
+
+if __name__ == "__main__":
+    teste1 = [1, 2, 2, 6, 6, 6, 6, 7, 10]
+    print(f"O elemento encontrado para teste1: {teste1}")
+    print(f"foi {mais_que_vinte_cinco_porcento(teste1)}")
