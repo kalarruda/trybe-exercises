b89f9e0ed2e6f1e9b30bd3a526aa91fb3c2d397d
José Carlos <kalarruda@hotmail.com>
2022-04-10 13:44:04 -0300

    exer

---
+++ modulo_4/bloco_37/dia_2/aula_gravada.py
@@ -0,0 +1,8 @@
+from collections import deque
+
+fila = deque()
+limite = deque([], 2)
+# limite = deque([], 2) define tamanho da fila
+# se inserir mais no limite ele tira o primeiro
+# fila.append('algo') adiciona no fim
+# fila.appendleft('algo2') adiciona no inicio
